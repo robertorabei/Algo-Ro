7a5c3266234c0bf579fe2a8b4cf9d04cacfd0883
Roberto Rabei <roberto.rabei@ulb.be>
2025-12-09 22:44:47 +0100

    added coms

--- src/benchmark.py
+++ src/benchmark.py
@@ -7,20 +7,26 @@ from solvers import extraire, solve_mtz, solve_dfj_it, solve_dfj_enum
 INSTANCES_DIR = "../instances"
 OUTPUT_CSV = "results.csv"
 
+REGEX_N = r"instance_(\d+)_"
+
 def benchmark():
     with open(OUTPUT_CSV, mode='w', newline='') as f:
         writer = csv.writer(f)
-        header = ["instance", "formulation", "obj_int", "time_int", "obj_relax", "time_relax", "gap", "vars", "constr"]
+        header = ["instance", "formulation", "obj_int", "time_int", "obj_relax", "time_relax", "gap", "vars", "constr", "iterations"]
         writer.writerow(header)
 
         files = [f for f in os.listdir(INSTANCES_DIR)]
+        files.sort(key=lambda x: (int(re.search(REGEX_N, x).group(1)), x))
 
         for filename in files:
             filepath =  os.path.join(INSTANCES_DIR, filename)
-
-
+            match = re.search(REGEX_N, filename)
+            if not match: continue
+            
             n, distances = extraire(filepath)
+            print(f"--- Traitement de {filename} (n={n}) ---")
             instance_name = filename.replace('.txt', '')
+            
             # MTZ 
             print(f"  > MTZ Integer...")
             obj_int, _, time_int, vars_int, constr_int = solve_mtz(distances, relaxed=False)
@@ -29,13 +35,13 @@ def benchmark():
             gap = (obj_int - obj_relax) / obj_int if obj_int else 0.0
             writer.writerow([instance_name, "MTZ", round(obj_int, 2), round(time_int, 4), 
                              round(obj_relax, 2), round(time_relax, 4), round(gap, 4), 
-                             vars_int, constr_int])
+                             vars_int, constr_int, "-"])
             
             # DFJ Itératif
             print(f"  > DFJ Iterative...")
             obj_iter, _, time_iter, iter_count, vars_iter, constr_iter = solve_dfj_it(distances)
             writer.writerow([instance_name, "DFJ_iter", round(obj_iter, 2), round(time_iter, 4), 
-                             "-", "-", "-", vars_iter, constr_iter])
+                             "-", "-", "-", vars_iter, constr_iter, iter_count])
             
             # DFJ Enumératif
             if n < 20:
@@ -46,7 +52,7 @@ def benchmark():
                 gap_enum = (obj_enum - obj_relax_enum) / obj_enum if obj_enum else 0.0
                 writer.writerow([instance_name, "DFJ_enum", round(obj_enum, 2), round(time_enum, 4), 
                                  round(obj_relax_enum, 2), round(time_relax_enum, 4), round(gap_enum, 4), 
-                                 vars_enum, constr_enum])
+                                 vars_enum, constr_enum, "-"])
             else:
                 print(f"  > DFJ Enum ignoré (n={n} >= 20)")
 
--- src/solvers.py
+++ src/solvers.py
@@ -3,74 +3,92 @@ from itertools import combinations
 import time
 
 def extraire(fichier: str) -> tuple[int, dict[int, tuple[float, float]], list[list[float]]]:
+    """
+    Extrait les données importantes d'une instance 
+    Args: 
+        fichier (str): chemin vers le fichier de l'instance
+    Returns:
+        n (int): nombre de villes
+        distances (list[list[float]]): matrice des distances entre les villes
+    """
     distances = []
     with open(fichier, 'r') as f:
-        # Lire n
+        # nombre de villes
         n = int(f.readline().strip())
-
-        # Skip les coordonnées des villes 
+        # skip les coordonnées
         for i in range(n):
-            ligne = f.readline()
-        
-        # Lire la matrice de distances
-        for _ in range(n):
-            ligne = f.readline().strip()
-            row = [float(v) for v in ligne.split()]
+            _ = f.readline()
+        # matrice des distances
+        for _ in range(n):  
+            line = f.readline().strip()
+            row = [float(v) for v in line.split()]
             distances.append(row)
-
     return n, distances
 
-def solve_mtz(distances: list[list[float]], relaxed: bool = False):
+def solve_mtz(distances: list[list[float]], relaxed: bool = False) -> tuple[float, list[int], float, int, int]:
+    """
+    Résout le TSP en utilisant la formulation MTZ relaxée ou non.
+    Args:
+        distances (list[list[float]]): matrice des distances entre les villes
+        relaxed (bool): si True, utilise la version relaxée
+    Returns:
+        obj_value (float): valeur de la fonction objective
+        tour (list[int]): liste des villes dans l'ordre du tour
+        solve_time (float): temps de résolution en secondes
+        num_vars (int): nombre de variables dans le modèle
+        num_constr (int): nombre de contraintes dans le modèle
+    """
     n = len(distances)
     nodes = list(range(n))
 
+    # création du modèle
     prob_name = "MTZ_Relaxed" if relaxed else "MTZ"
     model = pulp.LpProblem(prob_name, pulp.LpMinimize)
 
+    # définition du type de variable
     cat_x = pulp.LpContinuous if relaxed else pulp.LpBinary
     cat_u = pulp.LpContinuous if relaxed else pulp.LpInteger
 
-    # Variables de décision
-    # x_ij : 1 si l'arc (i,j) est utilisé, 0 sinon (ou réel entre 0 et 1 si relaxé)
+    # variables de décision
     x = pulp.LpVariable.dicts("x", (nodes, nodes), lowBound=0, upBound=1, cat=cat_x)
-    # u_i : variable auxiliaire pour l'ordre de visite
     u = pulp.LpVariable.dicts("u", nodes, lowBound=1, upBound=n, cat=cat_u)
 
-    # Fonction objectif
+    # fonction objectif
     model += pulp.lpSum(distances[i][j] * x[i][j] for i in nodes for j in nodes if i != j)
 
-    # Contraintes
-    # Pas de boucle sur soi-même
+    # contraintes
+    # pas de boucle sur soi-même
     for i in nodes:
         model += x[i][i] == 0
 
-    # Une sortie par ville (Contrainte de degré sortant)
+    # une sortie par ville (Contrainte de degré sortant)
     for i in nodes:
         model += pulp.lpSum(x[i][j] for j in nodes if i != j) == 1
 
-    # Une entrée par ville (Contrainte de degré entrant)
+    # une entrée par ville (Contrainte de degré entrant)
     for j in nodes:
         model += pulp.lpSum(x[i][j] for i in nodes if i != j) == 1
     
-    # Élimination des sous-tours (MTZ)
+    # élimination des sous-tours (MTZ)
     model += u[0] == 1
-
     for i in nodes:
         for j in nodes:
             if i != j and i != 0 and j != 0:
                 model += u[i] - u[j] + n * x[i][j] <= n - 1
 
+    # time le solveur
     start_time = time.time()
     status = model.solve(pulp.PULP_CBC_CMD(msg=False))
     solve_time = time.time() - start_time
 
+    # vérifie l'optimalité
     if pulp.LpStatus[status] != "Optimal":
         return None, None, solve_time, model.numVariables(), model.numConstraints()
 
     obj_value = pulp.value(model.objective)
     
+    # reconstitue le tour si pas relaxé
     tour = []
-    # Reconstruction du tour à partir des variables x si non relaxé car sinon les valeurs sont fractionnaires
     if not relaxed:
         selected = [(i, j) for i in nodes for j in nodes if pulp.value(x[i][j]) > 0.5]
         succ = {i: j for i, j in selected}
@@ -87,7 +105,15 @@ def solve_mtz(distances: list[list[float]], relaxed: bool = False):
 
     return obj_value, tour, solve_time, model.numVariables(), model.numConstraints()
 
-def find_cycles(x_solution, n):
+def find_cycles(x_solution, n) -> list[list[int]]:
+    """
+    Trouve tous les cycles dans la solution donnée avec BFS.
+    Args:
+        x_solution (dict): dictionnaire des variables de décision x_ij avec leurs valeurs
+        n (int): nombre de villes
+    Returns:
+        cycles (list[list[int]]): liste des cycles trouvés
+    """
     visited = [False] * n
     cycles = []
 
@@ -110,16 +136,29 @@ def find_cycles(x_solution, n):
     
     return cycles
 
-def dfj_model(n, distances):
+def dfj_model(n, distances) -> tuple[pulp.LpProblem, dict[tuple[int, int], pulp.LpVariable]]:
+    """
+    Crée le modèle DFJ pour la formulation de base sans contraintes de sous-tours.
+    Args:
+        n (int): nombre de villes
+        distances (list[list[float]]): matrice des distances entre les villes
+    Returns:
+        model (pulp.LpProblem): modèle linéaire
+        x (dict[tuple[int, int], pulp.LpVariable]): variables de décision x_ij
+    """
     nodes = list(range(n))
     model = pulp.LpProblem("DFJ", pulp.LpMinimize)
 
+    # variables
     x = pulp.LpVariable.dicts(
         "x", ((i,j) for i in nodes for j in nodes if i != j),
         cat="Binary"
     )
 
+    # fonction objectif
     model += pulp.lpSum(distances[i][j] * x[(i,j)] for i in nodes for j in nodes if i != j)
+    
+    # contraintes de degré 
     for i in nodes: 
         model += pulp.lpSum(x[(i,j)] for j in nodes if j != i) == 1
 
@@ -128,53 +167,86 @@ def dfj_model(n, distances):
 
     return model, x
 
-def solve_dfj_it(distances: list[list[float]]):
+def solve_dfj_it(distances: list[list[float]]) -> tuple[float, list[int], float, int, int, int]:
+    """
+    Résout le TSP en utilisant la formulation DFJ itérative.
+    Args:
+        distances (list[list[float]]): matrice des distances entre les villes
+    Returns:
+        obj_value (float): valeur de la fonction objective
+        tour (list[int]): liste des villes dans l'ordre du tour
+        total_solve_time (float): temps de résolution total en secondes
+        iteration (int): nombre d'itérations effectuées
+        num_vars (int): nombre de variables dans le modèle
+        num_constr (int): nombre de contraintes dans le modèle     
+    """
     n = len(distances)
     
+    # importation du modèle de base
     model, x = dfj_model(n, distances)
     
+    # variables pour le suivi du temps et des itérations
     total_solve_time = 0.0
     iteration = 0
     
     while True:
         iteration += 1
 
+        # addition des temps de résolution
         start_t = time.time()
         status = model.solve(pulp.PULP_CBC_CMD(msg=False))
         end_t = time.time()        
         total_solve_time += (end_t - start_t)
 
+        # vérification de l'optimalité
         if pulp.LpStatus[status] != "Optimal":
             print(f"Statut non optimal à l'itération {iteration}")
             return None, None, total_solve_time, iteration, model.numVariables(), model.numConstraints()
 
+        # extraction de la solution
         x_solution = {(i, j): pulp.value(x[(i, j)]) for (i, j) in x}
         
+        # recherche des cycles
         cycles = find_cycles(x_solution, n)
         
+        # si un cycle hamiltonien est trouvé, on termine
         if len(cycles) == 1 and len(cycles[0]) == n:
             tour = cycles[0]
             tour.append(tour[0])
             obj_value = pulp.value(model.objective)
             return obj_value, tour, total_solve_time, iteration, model.numVariables(), model.numConstraints()
 
+        # sinon, on ajoute les contraintes d'élimination de sous-tours
         for S in cycles:
             if len(S) < n:
                 model += (
                     pulp.lpSum(x[(i, j)] for i in S for j in S if i != j) <= len(S) - 1
                 )
 
-def solve_dfj_enum(distances: list[list[float]], relaxed: bool = False):
+def solve_dfj_enum(distances: list[list[float]], relaxed: bool = False) -> tuple[float, list[int], float, int, int]:
+    """ 
+    Résout le TSP en utilisant la formulation DFJ avec génération a priori des contraintes.
+    Args:
+        distances (list[list[float]]): matrice des distances entre les villes
+        relaxed (bool): si True, utilise une relaxation continue des variables
+    Returns:
+        obj_value (float): valeur de la fonction objective
+        tour (list[int]): liste des villes dans l'ordre du tour
+        solve_time (float): temps de résolution en secondes
+        num_vars (int): nombre de variables dans le modèle
+        num_constr (int): nombre de contraintes dans le modèle
+    """
     n = len(distances)
     nodes = list(range(n))
 
+    # création du modèle
     prob_name = "DFJ_Enum_Relaxed" if relaxed else "DFJ_Enum_Integer"
     model = pulp.LpProblem(prob_name, pulp.LpMinimize)
 
-    # Définition du type de variable
+    # type de variable
     cat_x = pulp.LpContinuous if relaxed else pulp.LpBinary
 
-    # Variables x_ij
+    # variables de décision
     x = pulp.LpVariable.dicts(
         "x",
         ((i, j) for i in nodes for j in nodes if i != j),
@@ -183,28 +255,29 @@ def solve_dfj_enum(distances: list[list[float]], relaxed: bool = False):
         cat=cat_x
     )
 
-    # Fonction objectif
+    # fonction objectif
     model += pulp.lpSum(distances[i][j] * x[(i, j)] for i in nodes for j in nodes if i != j)
     
-    # Contraintes de degré
+    # contraintes de degré
     for i in nodes:
         model += pulp.lpSum(x[(i, j)] for j in nodes if j != i) == 1
     for j in nodes:
         model += pulp.lpSum(x[(i, j)] for i in nodes if i != j) == 1
 
-    # Contraintes d'élimination de sous-tours (Génération a priori)
+    # contraintes d'élimination de sous-tours 
     for k in range(2, n):
-        # Pour tout sous-ensemble S de taille k
+        # pour tout sous-ensemble S de taille k
         for S in combinations(nodes, k):
             model += pulp.lpSum(x[(i, j)] for i in S for j in S if i != j) <= k - 1
 
-    # Mesure du temps solveur
+    # mesure du temps solveur
     start_time = time.time()
     status = model.solve(pulp.PULP_CBC_CMD(msg=False))
     solve_time = time.time() - start_time
 
     obj_value = pulp.value(model.objective)
-    
+
+    # reconstitue le tour si pas relaxé et optimal    
     tour = []
     if not relaxed and pulp.LpStatus[status] == "Optimal":
         selected = [(i, j) for (i, j) in x if pulp.value(x[(i, j)]) > 0.5]
